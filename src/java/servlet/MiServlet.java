05bceadf0aa61001b1c680025dd6ce7f07cba632
redes1 redes1 <redes1@SURCO.com>
2020-02-14 14:46:03 -0500

    primer commit

---
+++ src/java/servlet/MiServlet.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class MiServlet extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+           String usuario= request.getParameter("usuario");
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Mi Titulo</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hola "+usuario+" Bienvenido</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+  
+ 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+   
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+ 
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
